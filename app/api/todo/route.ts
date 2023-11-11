24531a5a1cfbbe5eb7a372c4ff533f2ddb94003d
amir-khaleghi <a.khaleghi.d@gmail.com>
2023-11-11 18:09:29 +0330

    middleware bug

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -2,10 +2,12 @@ import { NextResponse } from 'next/server';
 // import database
 import db from '@/utils/db';
 
+/* Get __________________________________________________ */
 export const GET = async (request: Request) => {
   const todos = await db.todo.findMany({});
   return NextResponse.json({ data: todos });
 };
+/* Post _________________________________________________ */
 export const POST = async (request: Request) => {
   const data = await request.json();
   const todo = await db.todo.create({
